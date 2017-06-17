c967aa543f71da8b574a425e62d8935df5b6cc9b
艾贺 <aihehe123@foxmail.com>
2017-06-17 17:15:21 +0800

    搭建框架

---
+++ SportStory/SportStory/AHDynamicViewController.h
@@ -0,0 +1,14 @@
+//
+//  AHDynamicViewController.h
+//  SportStory
+//
+//  Created by ai he on 2017/6/17.
+//  Copyright © 2017年 ai he. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AHBasicViewController.h"
+
+@interface AHDynamicViewController : AHBasicViewController
+
+@end
